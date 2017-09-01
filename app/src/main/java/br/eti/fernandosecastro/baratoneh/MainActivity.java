f9a268b826baf2c8104cbb15ca2f20e71ebef501
Fernando Se castro <fernandosecastro@univem.edu.br>
2017-09-01 08:14:16 -0300

    ds
    dsa

--- app/src/main/java/br/eti/fernandosecastro/baratoneh/MainActivity.java
+++ app/src/main/java/br/eti/fernandosecastro/baratoneh/MainActivity.java
@@ -1,5 +1,7 @@
 package br.eti.fernandosecastro.baratoneh;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,18 +82,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_produto) {
+        } else if (id == R.id.nav_listadesejo) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_mercado) {
+            Intent abreCadastro = new Intent(MainActivity.this,MercadoActivity.class);
+            startActivity(abreCadastro);
+            return true;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
